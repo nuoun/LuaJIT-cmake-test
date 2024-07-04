8e3cba133041e7b286cf86b8afc46a0d59919cca
nuoun <10226424+nuoun@users.noreply.github.com>
2024-07-04 21:03:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,17 @@ extern "C" {
 int main() {
     // Create Lua state
     lua_State* L = luaL_newstate();
-    
+
     // Load Lua libraries
     luaL_openlibs(L);
-    
-    // Run Lua
+
+    // Define script
     const char* script = R"(
 print(_VERSION)
 print(jit.version)
 )";
+
+    // Run Lua
     if (luaL_dostring(L, script) != LUA_OK) {
         std::cerr << "Error running Lua script: " << lua_tostring(L, -1) << std::endl;
         lua_pop(L, 1);
@@ -25,6 +27,6 @@ print(jit.version)
 
     // Close Lua state
     lua_close(L);
-    
+
     return 0;
 }
