0ef8d89425fc7996149c6a400192859c604079af
nuoun <10226424+nuoun@users.noreply.github.com>
2024-07-08 17:13:50 +0200

    update mac build to use amalg

--- libs/LuaJitLib/build-fat-luajit.sh
+++ libs/LuaJitLib/build-fat-luajit.sh
@@ -21,11 +21,11 @@ cp -r LuaJIT/* "${SD}"
 
 cd "${SD}/src"
 
-MACOSX_DEPLOYMENT_TARGET=10.9 make -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3"  || echo "That's OK though"
+MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3"  || echo "That's OK though"
 mv lib*a "${OD}/arm64"
 
 MACOSX_DEPLOYMENT_TARGET=10.9 make clean
-MACOSX_DEPLOYMENT_TARGET=10.9 make -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3"  || echo "That's OK though"
+MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3"  || echo "That's OK though"
 mv lib*a "${OD}/x86_64"
 
 lipo -create -arch arm64 "${OD}/arm64/libluajit.a" -arch x86_64 "${OD}/x86_64/libluajit.a" -output "${OD}/libluajit.a" || exit 17
